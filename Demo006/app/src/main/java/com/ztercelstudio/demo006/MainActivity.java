47035233b1405b7e66c02bbcef1cb387903f6e95
ztercel <ztercel@163.com>
2019-11-24 23:43:52 +0800

    UI四个常用布局

---
+++ Demo006/app/src/main/java/com/ztercelstudio/demo006/MainActivity.java
@@ -0,0 +1,24 @@
+package com.ztercelstudio.demo006;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button button = (Button)v;
+        Toast.makeText(this, button.getText(), Toast.LENGTH_SHORT).show();
+    }
+}
