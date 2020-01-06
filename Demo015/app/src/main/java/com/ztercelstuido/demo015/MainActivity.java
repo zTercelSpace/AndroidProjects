863779bbff076aef5b44154a70e10f71725f57a1
ztercel@163.com <ztercel@163.com>
2020-01-07 00:07:06 +0800

    Demo015 测试ByteBuffer

--- Demo015/app/src/main/java/com/ztercelstuido/demo015/MainActivity.java
+++ Demo015/app/src/main/java/com/ztercelstuido/demo015/MainActivity.java
@@ -26,10 +26,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         byte d = (byte)0xAA;
 
-        byte[] data = new byte[]{(byte)0xAA, (byte)0x55, (byte)0x00, (byte)0x03, (byte)0x50, (byte)0x01, (byte)0xAC};
+        byte[] data = new byte[]{(byte)0x00, (byte)0x03, (byte)0x50, (byte)0xAA, (byte)0x55, (byte)0x03, (byte)0x00, (byte)0x50, (byte)0x01, (byte)0xAC, (byte)0xDD};
 
         ByteBuffer dataBuffer = ByteBuffer.wrap(data);
 
+        byte[] head = new byte[]{(byte)0xAA, (byte)0x55};
+
+        //dataBuffer.flip();
+        /*
+        byte tmp1 = (byte) (dataBuffer.get() & 0xFF);
+        dataBuffer.mark();
+        byte tmp2 = (byte) (dataBuffer.get() & 0xFF);
+        byte tmp3 = (byte) (dataBuffer.get() & 0xFF);
+        dataBuffer.reset();
+        byte tmp4 = (byte) (dataBuffer.get() & 0xFF); */
+        //return;
+
+        for (int ii = 0; ii < dataBuffer.limit() - head.length; ii++) {
+            byte tmp = (byte) (dataBuffer.get() & 0xFF);
+            dataBuffer.mark();
+            // find head
+            boolean isFound = true;
+            for (int jj = 0; jj < head.length; jj++) {
+                if (tmp == head[jj]) {
+                    tmp = (byte)(dataBuffer.get() & 0xFF);
+                } else {
+                    dataBuffer.reset();
+                    isFound = false;
+                    break;
+                }
+            }
+
+            if (isFound) {
+                  Log.d("zTercel", "pos " + ii);
+
+                  //dataBuffer.reset();
+                  short frameLength = dataBuffer.getShort();
+                  byte[] content = new byte[frameLength];
+                  dataBuffer.get(content);
+                //  dataBuffer.compact();
+            }
+        }
+
+        return;
+        /*
         byte head1  = (byte)(dataBuffer.get() & 0xFF);
         byte head2  = (byte)(dataBuffer.get() & 0xFF);
         short length = (short)(dataBuffer.getShort());
@@ -40,8 +80,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         String shead1 = Integer.toHexString(head1).toUpperCase();
         String shead2 = Integer.toHexString(head2).toUpperCase();
-        Log.d("zTercel", shead1 + " " + shead2 + " " + length);
-        return;
+        Log.d("zTercel", shead1 + " " + shead2 + " " + length);*/
     }
 
     @Override
