fcd5bd4c84c4732d219c4c194b4e39340a08aeb8
Gogs <gogs@fake.local>
2020-01-10 19:56:35 +0800

    封装串口So库-实现串口协议帧提取并分发到解析器

--- Demo015/app/src/main/java/com/ztercelstuido/demo015/MainActivity.java
+++ Demo015/app/src/main/java/com/ztercelstuido/demo015/MainActivity.java
@@ -19,68 +19,216 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     };
 
+    class EventEntity {
+        byte      mID;
+        byte[]    mData;
+    }
+
+    interface IFrameDecoder {
+        class FrameInfo {
+            int     headPos;    // 帧的起始位置
+            int     tailPos;    // 帧的结束位置
+            int     length;     // 帧的长度
+        }
+
+        boolean decode(byte[] bytes, int dataBytes, FrameInfo frameInfo);
+    }
+
+    class FixedHeadLengthFrame {
+        private byte[]  mHead;
+        private int     mLengthFieldOffset;
+        private int     mLengthFieldBytes;
+
+        FixedHeadLengthFrame(byte[] head, int lengthFieldOffset, int lengthFieldBytes) {
+            if (head.length > 0) {
+                mHead = new byte[head.length];
+                System.arraycopy(head, 0, mHead, 0, head.length);
+            }
+
+            assert (lengthFieldOffset <= 0 || mLengthFieldBytes <= 0);
+
+            mLengthFieldOffset   = lengthFieldOffset;
+            mLengthFieldBytes    = lengthFieldBytes;
+        }
+    }
+
+    class FixedHeadLengthDecoder implements IFrameDecoder {
+
+        FixedHeadLengthFrame mFrameDes;
+
+        FixedHeadLengthDecoder(FixedHeadLengthFrame frameDes) {
+            mFrameDes = frameDes;
+        }
+
+        @Override
+        public boolean decode(byte[] data, int dataBytes, FrameInfo frameInfo) {
+            //-----------------------------------------------------------------
+            // frame:　head flag + lengthfield + data
+            //-----------------------------------------------------------------
+            boolean isDecoded = false;
+            if (dataBytes > (mFrameDes.mLengthFieldOffset + mFrameDes.mLengthFieldBytes)) {
+                int frameHeadPos    = findFrameHeadPos(data, mFrameDes.mHead);
+                int dataFiledBytes  = byteArrayToInt(data, frameHeadPos + mFrameDes.mLengthFieldOffset,mFrameDes. mLengthFieldBytes);
+                if ((-1 != frameHeadPos) && (0 < dataFiledBytes)) {
+                    int frameTailPos = frameHeadPos + mFrameDes.mLengthFieldOffset + mFrameDes.mLengthFieldBytes + dataFiledBytes;
+                    if (dataBytes >= frameTailPos) {
+                        frameInfo.headPos   = frameHeadPos;
+                        frameInfo.tailPos   = frameTailPos;
+                        frameInfo.length    = frameTailPos - frameHeadPos;
+
+                        isDecoded = true;
+                    }
+                }
+            }
+
+            return isDecoded;
+        }
+
+        int findFrameHeadPos(final byte[] data, byte[] head) {
+            int frameHeadPos = -1;
+            boolean isFound  = false;
+
+            for (int ii = 0; ii < data.length && !isFound; ii++) {
+                frameHeadPos = ii;
+                for (int jj = 0; jj < head.length; jj++) {
+                    if (data[ii + jj] == head[jj]) {
+                        isFound = true;
+                    } else {
+                        isFound = false;
+                        break;
+                    }
+                }
+            }
+
+            return isFound ? frameHeadPos : -1;
+        }
+
+        int byteArrayToInt(final byte[] bytes, int offset, int length) {
+            if ((bytes.length < (offset + length)) || length > 4 || 0 >= length) {
+                return -1;
+            }
+
+            byte[] lengthFieldBytes = new byte[length];
+            System.arraycopy(bytes, offset, lengthFieldBytes, 0, length);
+
+            return byteArrayToInt(lengthFieldBytes);
+        }
+
+        int byteArrayToInt(byte[] bytes) {
+
+            if (bytes.length > 4 || 0 == bytes.length) {
+                return 0;
+            }
+
+            byte[] intBytes = new byte[4];
+            int index = intBytes.length - 1;
+            for (int pos = bytes.length - 1; pos >= 0; pos--, index--) {
+                intBytes[index] = bytes[pos];
+            }
+
+            int val0 = (intBytes[0] & 0xFF) << 24;
+            int val1 = (intBytes[1] & 0xFF) << 16;
+            int val2 = (intBytes[2] & 0xFF) << 8;
+            int val3 = (intBytes[3] & 0xFF) << 0;
+
+            return (val0 + val1 + val2 + val3);
+        }
+    }
+
+    interface IFrameParser {
+        void parse(ByteBuffer frame);
+    }
+
+    class VehicleFrameParser implements IFrameParser {
+
+        private int frameCount = 0;
+
+        public void parse(ByteBuffer frame) {
+            if (frame.limit() > 6 && checkSum(frame)) {
+
+                frameCount++;
+                Log.d("zTercel", "FrameParser::parse -- " + frameCount);
+            } else {
+                Log.d("zTercel", "FrameParser::parse -- ");
+            }
+        }
+
+        boolean checkSum(ByteBuffer frame) {
+            frame.position(2);
+
+            byte sum = 0;
+            byte[] bytes = frame.array();
+            for (int ii = 2; ii < bytes.length - 1; ii++) {
+                sum += (byte)(bytes[ii] & 0xFF);
+            }
+            sum ^= 0xff;
+            sum += 1;
+
+            int checkSum = (byte)(bytes[bytes.length - 1] & 0xFF);
+            return (sum == checkSum);
+        }
+    }
+
+    class DataHandler {
+
+        private IFrameParser    mFrameParser;
+        private IFrameDecoder  mFrameDecorder;
+        private ByteBuffer      mByteBuffer;
+
+        DataHandler(IFrameDecoder frameDecorder, IFrameParser frameParser) {
+            mFrameParser    = frameParser;
+            mFrameDecorder  = frameDecorder;
+
+            mByteBuffer     = ByteBuffer.allocate(1024);
+        }
+
+        void onHandle(byte[] data) {
+            mByteBuffer.put(data);
+
+            IFrameDecoder.FrameInfo frameInfo = new IFrameDecoder.FrameInfo();
+
+            mByteBuffer.flip();
+            while (mFrameDecorder.decode(mByteBuffer.array(), mByteBuffer.limit(), frameInfo)) {
+                mByteBuffer.position(frameInfo.headPos);
+                byte[] frame = new byte[frameInfo.length];
+                mByteBuffer.get(frame);
+
+                if (null != mFrameParser) {
+                    mFrameParser.parse(ByteBuffer.wrap(frame));
+                }
+                mByteBuffer.compact();
+                mByteBuffer.flip();
+            }
+            mByteBuffer.compact();
+
+            if (mByteBuffer.position() > mByteBuffer.capacity() / 5 * 4) {
+                mByteBuffer.clear();
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        byte d = (byte)0xAA;
+        byte[] HEAD_TAG = {(byte)0xAA, (byte)0x55};
+        FixedHeadLengthFrame frameDes = new FixedHeadLengthFrame(HEAD_TAG, 2, 2);
+        IFrameDecoder   decoder = new FixedHeadLengthDecoder(frameDes);
+        IFrameParser    parser  = new VehicleFrameParser();
 
-        byte[] data = new byte[]{(byte)0x00, (byte)0x03, (byte)0x50, (byte)0xAA, (byte)0x55, (byte)0x03, (byte)0x00, (byte)0x50, (byte)0x01, (byte)0xAC, (byte)0xDD};
+        byte[][] data = {
+                {(byte)0x03, (byte)0x50, (byte)0xAA, (byte)0x55, (byte)0x00, (byte)0x03, (byte)0x50, (byte)0x01, (byte)0xAC,
+                 (byte)0xAA, (byte)0x55, (byte)0x00, (byte)0x03, (byte)0x50, (byte)0x01, (byte)0xAC, (byte)0xAA, (byte)0x55},
+                {(byte)0x00, (byte)0x03, (byte)0x50, (byte)0x01, (byte)0xAC, (byte)0xAA, (byte)0x55, (byte)0x00, (byte)0x03,},
+                {(byte)0x50, (byte)0x01, (byte)0xAC, }
+        };
 
-        ByteBuffer dataBuffer = ByteBuffer.wrap(data);
+        DataHandler dataHandler = new DataHandler(decoder, parser);
 
-        byte[] head = new byte[]{(byte)0xAA, (byte)0x55};
-
-        //dataBuffer.flip();
-        /*
-        byte tmp1 = (byte) (dataBuffer.get() & 0xFF);
-        dataBuffer.mark();
-        byte tmp2 = (byte) (dataBuffer.get() & 0xFF);
-        byte tmp3 = (byte) (dataBuffer.get() & 0xFF);
-        dataBuffer.reset();
-        byte tmp4 = (byte) (dataBuffer.get() & 0xFF); */
-        //return;
-
-        for (int ii = 0; ii < dataBuffer.limit() - head.length; ii++) {
-            byte tmp = (byte) (dataBuffer.get() & 0xFF);
-            dataBuffer.mark();
-            // find head
-            boolean isFound = true;
-            for (int jj = 0; jj < head.length; jj++) {
-                if (tmp == head[jj]) {
-                    tmp = (byte)(dataBuffer.get() & 0xFF);
-                } else {
-                    dataBuffer.reset();
-                    isFound = false;
-                    break;
-                }
-            }
-
-            if (isFound) {
-                  Log.d("zTercel", "pos " + ii);
-
-                  //dataBuffer.reset();
-                  short frameLength = dataBuffer.getShort();
-                  byte[] content = new byte[frameLength];
-                  dataBuffer.get(content);
-                //  dataBuffer.compact();
-            }
+        for (int ii = 0; ii < data.length; ii++) {
+            dataHandler.onHandle(data[ii]);
         }
-
-        return;
-        /*
-        byte head1  = (byte)(dataBuffer.get() & 0xFF);
-        byte head2  = (byte)(dataBuffer.get() & 0xFF);
-        short length = (short)(dataBuffer.getShort());
-
-        if (((byte)(0xAA)) == head1) {
-            Log.d("zTercel", "" + length);
-        }
-
-        String shead1 = Integer.toHexString(head1).toUpperCase();
-        String shead2 = Integer.toHexString(head2).toUpperCase();
-        Log.d("zTercel", shead1 + " " + shead2 + " " + length);*/
     }
 
     @Override
